7825e636a429fec32917e3cff17802c0be5e513d
sanctionedpranav <pranav9728@gmail.com>
2025-03-28 16:47:20 +0530

    canvas setup ready

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import {Environment, Center} from "@react-three/drei";
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
-  )
-}
+  <Canvas>
 
-export default Canvas
+  </Canvas>
+  );
+};
+
+export default CanvasModel;
