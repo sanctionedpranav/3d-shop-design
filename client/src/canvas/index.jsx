33fea0634f054321dbdb23d301fb7660970ccb78
sanctionedpranav <pranav9728@gmail.com>
2025-03-31 15:39:58 +0530

    shirt and canvas is ready

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import {Environment, Center} from "@react-three/drei";
+import { Center, Environment } from "@react-three/drei";
+import CameraRig from "./CameraRig";
+import BackDrop from "./BackDrop";
+import Shirt from "./Shirt";
 
 const CanvasModel = () => {
   return (
-  <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full !h-screen transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
 
-  </Canvas>
+      <CameraRig>
+        <BackDrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
   );
 };
 
